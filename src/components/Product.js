028341b26a0cfb60d0cdc6da2d7eb3ac44cf428f
Dexter Woods Jr <dexterjuneyur@gmail.com>
2020-08-28 12:15:18 -0700

    card component

---
+++ src/components/Product.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container, Row, Col
+} from "reactstrap";
+
+const Product = (props) => {
+  return (
+    <div>
+        <Card>
+            <CardImg
+            src="https://picsum.photos/200/?blur"
+            alt="Card image cap"
+            />
+            <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </CardText>
+            <Button className="button">Add to Cart</Button>
+            </CardBody>
+        </Card>
+    </div>
+  );
+};
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Product from './components/Product';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render () {
+    return (
+          <div className="col-4 flex-wrap">
+            <Product />
+            <Product />
+            <Product />
+          </div>
+    );
+  }
+}
+
+render (<App />, document.getElementById('root'));
